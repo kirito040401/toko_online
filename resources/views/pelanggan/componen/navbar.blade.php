1b9fda8410ad87745da83caa41a126dd42c42c4e
kirito0404 <kirito.kirigaya.asuna12@gmail.com>
2024-01-14 01:46:51 +0700

    lanjutan untuk besok

--- resources/views/pelanggan/componen/navbar.blade.php
+++ resources/views/pelanggan/componen/navbar.blade.php
@@ -1,25 +1,27 @@
 <nav class="navbar navbar-dark navbar-expand-lg" style="background-color: #025464">
     <div class="container">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-            data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false"
-            aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03"
+            aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <a class="navbar-brand" href="#">Toko Online</a>
         <div class="collapse navbar-collapse justify-content-end gap-4" id="navbarTogglerDemo03">
             <ul class="navbar-nav gap-4">
                 <li class="nav-item">
-                    <a class="nav-link active {{ Request::path() == '/' ? 'active' : ''; }}" aria-current="page" href="/">Home</a>
+                    <a class="nav-link active {{ Request::path() == '/' ? 'active' : '' }}" aria-current="page"
+                        href="/">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active {{ Request::path() == 'shop' ? 'active':''; }}" href="/shop">Shop</a>
+                    <a class="nav-link active {{ Request::path() == 'shop' ? 'active' : '' }}" href="/shop">Shop</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active {{ Request::path() == 'contact' ? 'active':''; }}" href="/contact">Contact Up</a>
+                    <a class="nav-link active {{ Request::path() == 'contact' ? 'active' : '' }}" href="/contact">Contact
+                        Up</a>
                 </li>
             </ul>
             <form class="d-flex gap-4 align-items-center">
-                <button class="btn btn-success" type="submit">Login | Register</button>
+                <button type="button" class="btn btn-success" data-bs-toggle="modal"
+                    data-bs-target="#exampleModal">Login | Register</button>
                 <a href="/transaksi" class="fs-5 icon-nav">
                     <i class="fa-solid fa-bag-shopping"></i>
                 </a>
